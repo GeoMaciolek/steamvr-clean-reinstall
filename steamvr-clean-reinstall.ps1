9f23463065c77f556cd914aac586d23ddf8433c7
Geo Maciolek <geoffmaciolek@gmail.com>
2023-02-14 21:49:48 -0500

    Start Remove-Steamapp func., Fix path formatting

--- steamvr-clean-reinstall.ps1
+++ steamvr-clean-reinstall.ps1
@@ -1,12 +1,23 @@
 ﻿Param(
 	[array]
     $FilesToRemove = @(
-        "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\resources\settings\default.vrsettings",
-	    "C:\Program Files (x86)\Steam\config\steamvr.vrsettings",
-        "C:\Program Files (x86)\Steam\config\lighthouse\lighthousedb.json"),
+        'C:\Program Files (x86)\Steam\steamapps\common\SteamVR\resources\settings\default.vrsettings',
+        'C:\Program Files (x86)\Steam\config\steamvr.vrsettings',
+        'C:\Program Files (x86)\Steam\config\lighthouse\lighthousedb.json'),
     [int32]
     $SteamVRAppId = 250820,
+    [string]
+    $SteamExePath = 'C:\Program Files (x86)\Steam\steam.exe',
     [boolean]
 	$TestMode = $true
 )
 
+function Remove-SteamApp {
+    Param(
+        $SteamExePath = $SteamExePath,
+        $SteamVRAppId = $SteamVRAppId
+    )
+
+    # Launch steam.exe, telling it to uninstall the requested app
+    Start-Process -FilePath $SteamExePath -Wait -ArgumentList "steam://uninstall/$SteamVRAppId"
+}
