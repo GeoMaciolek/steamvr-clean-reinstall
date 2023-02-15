c65997e0caa28b06f182f35ae4cc630dd0404210
Geo Maciolek <geoffmaciolek@gmail.com>
2023-02-14 22:11:00 -0500

    Modularize steampath, set up file del. & uninstall

--- steamvr-clean-reinstall.ps1
+++ steamvr-clean-reinstall.ps1
@@ -1,13 +1,15 @@
 ﻿Param(
+    [string]
+    $SteamPath = ${env:ProgramFiles(x86)} + '\Steam',
 	[array]
     $FilesToRemove = @(
-        'C:\Program Files (x86)\Steam\steamapps\common\SteamVR\resources\settings\default.vrsettings',
-        'C:\Program Files (x86)\Steam\config\steamvr.vrsettings',
-        'C:\Program Files (x86)\Steam\config\lighthouse\lighthousedb.json'),
+        "$SteamPath\steamapps\common\SteamVR\resources\settings\default.vrsettings";
+        "$SteamPath\Steam\config\steamvr.vrsettings";
+        "$SteamPath\config\lighthouse\lighthousedb.json"),
     [int32]
     $SteamVRAppId = 250820,
     [string]
-    $SteamExePath = 'C:\Program Files (x86)\Steam\steam.exe',
+    $SteamExePath = "$SteamPath\steam.exe",
     [boolean]
 	$TestMode = $true
 )
@@ -15,9 +17,36 @@
 function Remove-SteamApp {
     Param(
         $SteamExePath = $SteamExePath,
-        $SteamVRAppId = $SteamVRAppId
+        $SteamVRAppId = $SteamVRAppId,
+        [boolean] $WhatIfSetting = $TestMode # "Inheret" testmode from main script
     )
-
+    $SplatParams = @{}
+    $SplatParams.WhatIf = $WhatIfSetting
     # Launch steam.exe, telling it to uninstall the requested app
-    Start-Process -FilePath $SteamExePath -Wait -ArgumentList "steam://uninstall/$SteamVRAppId"
+    Start-Process -FilePath $SteamExePath -Wait -ArgumentList "steam://uninstall/$SteamVRAppId" @SplatParams
 }
+
+function Remove-ConfigFiles {
+    Param(
+        $ConfigFilesToRemove = $FilesToRemove,
+        [boolean] $WhatIfSetting = $TestMode # "Inheret" testmode from main script
+    )
+    $SplatParams = @{}
+    $SplatParams.WhatIf = $WhatIfSetting
+    ForEach ($fileToRemove in $ConfigFilesToRemove) {
+        if (Test-Path $fileToRemove) {
+            If ($WhatIfSetting) {
+                Write-Output "We would be removing $file"
+            }
+            Write-Verbose "Removing $fileToRemove"
+            Remove-Item $fileToRemove @SplatParams
+        } else {
+            Write-Warning "File $fileToRemove does not exist, skipping"
+        }
+    }
+}
+# if $()
+# ForEach ($file in $FilesToRemove) {
+#     Write-Output "remove $file lol"
+#     write-output "----"
+# }
